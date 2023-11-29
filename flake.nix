723187bde0fd9967bc1f6d31bff14c50f3e220ff
Marco Burro <marcoburro98@gmail.com>
2023-11-29 23:44:09 +0100

    use poetry2nix

--- flake.nix
+++ flake.nix
@@ -4,17 +4,31 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
-    let pkgs = nixpkgs.legacyPackages.${system}; in
-    {
-      devShells.default = pkgs.mkShellNoCC {
-        buildInputs = [
-          pkgs.python3
-          pkgs.poetry
-        ];
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryEnv mkPoetryApplication;
+      gingerino = mkPoetryApplication {
+        projectDir = self;
+        python = pkgs.python311;
+        preferWheels = true;
+        checkPhase = ''
+          runHook preCheck
+
+          python -m pytest
+
+          runHook postCheck
+        '';
       };
+    in
+    {
+      packages.default = gingerino;
     }
   );
 }
