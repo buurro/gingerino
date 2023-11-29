fbcb854088b3b15b28618e80dc58a5a1475f4b85
Marco Burro <marcoburro98@gmail.com>
2023-11-29 23:53:09 +0100

    broken test

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
+from decimal import Decimal
 
-# from decimal import Decimal
 import pytest
 
 from gingerino import Gingerino
@@ -26,13 +26,13 @@ def test_invalid_property():
     assert str(error.value) == "'stuff' is not valid"
 
 
-# def test_missing_property():
-#     @dataclass
-#     class Drink:
-#         name: str
-#         price: Decimal
+def test_missing_property():
+    @dataclass
+    class Drink:
+        name: str
+        price: Decimal
 
-#     with pytest.raises(TemplateError) as error:
-#         Gingerino(Drink, "{{ name }}")
+    with pytest.raises(TemplateError) as error:
+        Gingerino(Drink, "{{ name }}")
 
-#     assert str(error.value) == "required field 'price' is missing"
+    assert str(error.value) == "required field 'price' is missing"
