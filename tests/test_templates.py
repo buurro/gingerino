73fe4fd353e3c737050bbeb232439699704b8282
Marco Burro <marcoburro98@gmail.com>
2023-11-29 23:44:31 +0100

    some tests

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -1,7 +1,18 @@
-import pytest
 from dataclasses import dataclass
+
+# from decimal import Decimal
+import pytest
+
 from gingerino import Gingerino
 from gingerino.main import TemplateError
+
+
+def test_valid_template():
+    @dataclass
+    class Drink:
+        name: str
+
+    Gingerino(Drink, "{{ name }}")
 
 
 def test_invalid_property():
@@ -13,3 +24,15 @@ def test_invalid_property():
         Gingerino(Drink, "{{ name }} {{ stuff }}")
 
     assert str(error.value) == "'stuff' is not valid"
+
+
+# def test_missing_property():
+#     @dataclass
+#     class Drink:
+#         name: str
+#         price: Decimal
+
+#     with pytest.raises(TemplateError) as error:
+#         Gingerino(Drink, "{{ name }}")
+
+#     assert str(error.value) == "required field 'price' is missing"
